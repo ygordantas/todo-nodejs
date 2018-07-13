e4618a88dd9c83a17d5c0bddebb6ee8de6a7c03e
ygordantas <ygord.furtado@gmail.com>
2018-07-13 10:59:22 -0500

    Post Todos route created and Mongoose refactory

---
+++ server/modules/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  }
+});
+
+module.exports = { User };
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose");
+const { user } = require("./modules/user");
+const { Todo } = require("./modules/todo");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo
+    .save()
+    .then(doc => {
+      res.send(doc);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
+});
