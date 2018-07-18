0e1e63c0792c47be129de612c8488b9d47872814
ygordantas <ygord.furtado@gmail.com>
2018-07-18 11:38:20 -0500

    Getting error status is undefined

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,14 @@ const request = require("supertest");
 const { app } = require("./../server");
 const { Todo } = require("./../modules/todo");
 
+const todos = [{ text: "First" }, { text: "second" }];
+
 beforeEach(done => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -20,7 +26,7 @@ describe("POST /todos", () => {
       })
       .end((err, res) => {
         if (err) return done(err);
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -38,10 +44,22 @@ describe("POST /todos", () => {
         if (err) return done(err);
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(e => done(e));
       });
+  });
+});
+
+describe("GET /todos", () => {
+  it("Should get all todos", done => {
+    request(app)
+      .get("todos")
+      .expect(200)
+      .expect(res => {
+        expect(res.body.length).toBe(2);
+      })
+      .end(done);
   });
 });
